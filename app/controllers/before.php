1b3eaba8f15cb60c4bf128cd5fda528e066328b0
refirio <refirio@gmail.com>
2016-05-21 18:08:36 +0900

    ログイン確認処理を分割

--- app/controllers/before.php
+++ app/controllers/before.php
@@ -2,13 +2,3 @@
 
 //設定ファイル
 import('app/config.php');
-
-//ログイン確認
-if ($_REQUEST['mode'] === 'admin' && !preg_match('/^(index|logout)$/', $_REQUEST['work'])) {
-    if (empty($_SESSION['administrator']['id']) || localdate() - $_SESSION['administrator']['time'] > $GLOBALS['login_expire']) {
-        //リダイレクト
-        redirect('/admin/logout');
-    } else {
-        $_SESSION['administrator']['time'] = localdate();
-    }
-}
